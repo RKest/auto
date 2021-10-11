2a0068db7dcce596c4a5da4ad4d4f6ebc34818b4
unknown <RKest>
2021-10-11 13:22:22 +0200

    Fix

--- app.js
+++ app.js
@@ -2,46 +2,43 @@ const scraper = require("./modules/scraping");
 const parser = require("./modules/parsing");
 
 const express = require("express");
-const fs = require("fs/promises");
+const fs = require("fs");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-const CASHE_PATH = __dirname + "/public/index.html";
 const VIEW_PATH = __dirname + "/views";
+const CASHE_PATH = VIEW_PATH + "/table.html";
 
-(async () => {
-    app.route("/")
-    .get((_, res) => {
-        res.send(VIEW_PATH + "index.html");
+app.route("/") 
+.get((req, res) => {
+    console.log(req.method);
+    res.sendFile(VIEW_PATH + "/index.html");
+})
+.post((req, res) => {
+    const email = req.body.email;
+    const passwd = req.body.passwd;
+    const date = JSON.parse(req.body.date);
+    console.log(email, passwd, date);
+
+    scraper.scrape(email, passwd)
+    .then(obj => {
+        fs.writeFileSync(CASHE_PATH, parser.parse(obj), "utf-8");
+        res.statusCode = 200;
+        res.send("http://localhost:8080/data");
     })
-    .post((req, res) => {
-        const email = req.body.email;
-        const passwd = req.body.passwd;
-        const date = JSON.parse(req.body.date);
-        console.log(email, passwd, date);
-
-        scraper.scrape(email, passwd)
-        .then(obj => {
-            fs.writeFile(CASHE_PATH, parser.parse(obj), "utf-8")  
-        })
-        .then(() => {
-            res.statusCode = 200;
-            res.send("http://localhost:8080/data");
-        })
-        .catch(e => {
-            res.statusCode = 400;
-            res.send(e);
-        });
+    .catch(e => {
+        res.statusCode = 400;
+        res.send(e);
     });
-    app.get("/data", (_, res) => {
-        res.sendFile(CASHE_PATH);
-    });
-})();
+});
 
-app.listen(8080, () => {
-    console.log("http://localhost:8080");
+app.all("/data", (_, res) => {
+    res.sendFile(CASHE_PATH);
+});
+
+app.listen(80, () => {
+    console.log("http://localhost");
 });
 
