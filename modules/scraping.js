f39df5426f2c569f45440430524ae58b8ebaa08e
unknown <RKest>
2021-10-11 11:53:02 +0200

    Cleaning code and cradels of a gui

--- modules/scraping.js
+++ modules/scraping.js
@@ -2,37 +2,51 @@ const puppeteer = require("puppeteer");
 const util = require("util");
 const sleep = util.promisify(setTimeout);
 const LOCATION = "https://gero.icnea.net";
+const LOGIN_REDIRECT_LOCATION = LOCATION + "/Servidor.aspx";
 const DEF_LOCATION = LOCATION + "/HosOrdEntrades.aspx"
 
 const INDIFFERENCE_AMOUNT = 10.0;
 const DEPOSIT_AMOUNT = 150.0;
 const NO_TABLE_COLS = 14;
+const pageGotoOptions = { waitUntil: "networkidle2" };
 
-//const howManyNights = "#liNits";
 const initialOffset = 5;
 const tableDateTimeOffset = 0;
 const tablePropertyOffset = 1;
 
-const tablePassportNumberSelector = ".table-condensed > tbody > tr > td:nth-child(7)";
+//Login Page Selectors
+const emailInpSelector = "input#Email";
+const passwordInpSelector = "input#Contrasenya"
+const loginInpSelector = "input#Login";
+
+//Arivals Page Selectors
 const tableReqQuerySelectorAll = ".table-condensed > tbody > tr > td:nth-child(14n + 5) > a";
-const guestTableRowSelectorAll = ".table-condensed > tbody > tr";
-const doesHaveVirtualCardTextareaDeterminatorSelector = "textarea#obs_canal";
-const depositHasBenDeclaredDeterminantSelectorAll = ".table-condensed > tbody > tr";
-const hasPaidTheDepositCandidateSelector = "td.text-right:not(.text-danger):not(.form-inline):not([colspan='2'])";
-const toPayAmountSelector = ".warning > td > strong";
-const personsSelectors = ["input#personesG", "input#personesM", "input#personesP"];
-const outstandingPaymentSelector = "td > strong.text-danger";
+
+//Cleaning Page Selectors
+
+//Tab Selectors
 const bookingTabAnchorSelector = "a#Menus_M1";
 const guestsTabAnchorSelector = "a#Menus_M2";
+
+//Customer Tab Selectors
+const url_guest_appSelector = "a#App"; //UNUSED
 const departureDateSelector = "input#sortida";
 
-const url_guest_appSelector = "a#App";
-const pageGotoOptions = { waitUntil: "networkidle2" };
+//Guest Tab Selectors
+const guestTableRowSelectorAll = ".table-condensed > tbody > tr";
+const tablePassportNumberSelector = ".table-condensed > tbody > tr > td:nth-child(7)";
 
-const splitDateTime = dateString => {
-    const datePart = dateString.slice(0, 10).trim();
-    const timePart = dateString.slice(11).trim();
-    return [datePart, timePart];
+
+const splitDateTime = dateStringArray => {
+    const ret = [[], []];
+
+    for(const dateString of dateStringArray){
+        const datePart = dateString.slice(0, 10).trim();
+        const timePart = dateString.slice(11).trim();
+        ret[0].push(datePart);
+        ret[1].push(timePart);
+    }
+    return ret;
 }
 
 const awaitClick = async (el, page) => {
@@ -56,17 +70,97 @@ const nDaysFromGivenDate = (dateString, num) => {
     return date;
 }
 
+const noPersonsFunc = async page => {
+    const personsSels = ["input#personesG", "input#personesM", "input#personesP"];
+    var noPersons = 0;
+    for(const personsSel of personsSels)
+        noPersons += +(await page.$eval(personsSel, el => el.value));
+    return noPersons;
+}
+
+const hasVCFunc = async (page, service) => {
+    const doesHaveVCTextareaSel = "textarea#obs_canal";
+    return service === "expedia" || await page.$eval(doesHaveVCTextareaSel,  
+        el => /crédito virtual/.test(el.textContent.trim()));
+}
+
+const toPayAmountFunc = async page => {
+    const toPayNumSel = ".warning > td > strong";
+    return page.$eval(toPayNumSel, el => +el.textContent.trim().slice(0, -2));
+}
+
+const outstandingPaymentFunc = async page => {
+    const outstandingPaymentSel = "td > strong.text-danger";
+    return page.$eval(outstandingPaymentSel, el => +el.textContent.trim().slice(0,-2));
+}
+
+const hasDepositExtraFunc = async page => {
+    const depositBeenDeclaredSelAll = ".table-condensed > tbody > tr";
+    return page.$$eval(depositBeenDeclaredSelAll, els => {
+        const depositNames = ["deposit", "depósito", "deposito", "fianza"];
+        var ret = false;
+        for(var i = 0; i < els.length; i++){
+            const elsChildrenNodes = els[i].childNodes;
+            const elsTitleNode = elsChildrenNodes[1].textContent.trim();
+            const elsTitleNodeLC = elsTitleNode.toLowerCase();
+            ret |= depositNames.includes(elsTitleNodeLC);
+        }
+        return ret;
+    });
+}
+
+const hasPaidDepositFunc = (hasVC, toPayAmout, outstandingPayment, hasMadeDepositTransaction, hasDepositExtra) => {
+    const hasPaidWithoutVC = 
+        ( hasDepositExtra && Math.abs(outstandingPayment) < INDIFFERENCE_AMOUNT) ||
+        (!hasDepositExtra && Math.abs(outstandingPayment + DEPOSIT_AMOUNT) < INDIFFERENCE_AMOUNT);
+    const hasPaidWithVC = 
+        ( hasDepositExtra && Math.abs(toPayAmout - outstandingPayment) < INDIFFERENCE_AMOUNT) ||
+        (!hasDepositExtra && Math.abs(toPayAmout - outstandingPayment - DEPOSIT_AMOUNT) < INDIFFERENCE_AMOUNT);
+
+    return hasPaidWithoutVC || hasMadeDepositTransaction || (hasVC && hasPaidWithVC);
+};
+
+const hasDepositTransactionFunc = async (page, departureDate) => {
+    const hasPaidDepositSel = "td.text-right:not(.text-danger):not(.form-inline):not([colspan='2'])";
+    return page.$$eval(hasPaidDepositSel, async (els, departureDate) => {
+       const lastEl = els[els.length - 1];
+       const lastElNodes = lastEl.childNodes;
+       if(lastElNodes.length === 1)
+       {
+           const lastElParent = lastEl.parentNode;
+           const lastElParentFirstTd = lastElParent.childNodes[1];
+           const paymentDateAsString = lastElParentFirstTd.textContent.trim();
+           const paymentDateTrimmed = paymentDateAsString.slice(-10);
+           const paymentDatePlusSevenDays = await nDaysFromGivenDate(paymentDateTrimmed, 7);
+           const departureDateAsDate = await dateFromString(departureDate);
+           return paymentDatePlusSevenDays > departureDateAsDate;
+       }
+       return false;
+   }, departureDate);
+}
+
 const scrape = async () => {
     const browserURL = "http://127.0.0.1:9222";
     const browser = await puppeteer.connect({browserURL});
     const cleanApts = await cleanApartamentNames(browser);
     const page = await browser.newPage();
     await page.goto(DEF_LOCATION, pageGotoOptions);
+    const pageUrl = page.url();
+    if(pageUrl === LOGIN_REDIRECT_LOCATION){
+        if(!process.env.EMAIL || !process.env.PASSWORD)
+            throw "Provide email and password";
+        await Promise.all([
+            page.$eval(emailInpSelector,    (el, val) => el.value = val, process.env.EMAIL),
+            page.$eval(passwordInpSelector, (el, val) => el.value = val, process.env.PASSWORD)
+        ]);
+        await awaitClick(loginInpSelector, page);
+        if(page.url() === LOGIN_REDIRECT_LOCATION)
+            throw "Incorrect email or password";
+    }
 
     var Contents = [];
     var Headers = [];
     const reqQueries = await page.$$eval(tableReqQuerySelectorAll, els => els.map(el => el.href));
-    const reqTexts =  await page.$$eval(tableReqQuerySelectorAll, els => els.map(el => el.textContent.trim()));
     for(var i = initialOffset + 1; i < NO_TABLE_COLS; i++){
         const contentsSelectorAll = `.table-condensed > tbody > tr > td:nth-child(${NO_TABLE_COLS}n + ${i})`;
         const headersSelector = `.table-condensed > thead > tr > th:nth-child(${i})`;
@@ -78,14 +172,14 @@ const scrape = async () => {
 
     const services = Contents[Contents.length - 1];
     const properties = Contents[tablePropertyOffset];
-    const splitDatesList = Contents[tableDateTimeOffset].map(el => splitDateTime(el));
-    Contents = [splitDatesList.map(el => el[0]), splitDatesList.map(el => el[1]), ...Contents.slice(1)];
+    const splitDatesList = splitDateTime(Contents[tableDateTimeOffset]);
+    Contents = [...splitDatesList, ...Contents.slice(1)];
     Headers = ["Date", "Time", ...Headers.slice(1)];
 
     const hasGuestsFIlledOut = [];
     const hasPaidThePrices = [];
     const hasPaidDeposits = [];
-    const urlGuestApps = [];
+    //const urlGuestApps = [];
     const paymentDifferences = [];
     const areAllPassportsValids = [];
     const areTheApartmentClean = properties.map(el => cleanApts.some(apt => 
@@ -100,81 +194,34 @@ const scrape = async () => {
         await temp_page.exposeFunction("nDaysFromGivenDate", nDaysFromGivenDate);
         await temp_page.exposeFunction("dateFromString", dateFromString);
         
-        var noPersons = 0;
-        for(const personsSelector of personsSelectors)
-            noPersons += +(await temp_page.$eval(personsSelector, el => el.value));
+        const noPersons = await noPersonsFunc(temp_page);
 
         const departureDate = await temp_page.$eval(departureDateSelector, el => el.value);
-        const url_guest_app = await temp_page.$eval(url_guest_appSelector, el => el.href);
-        urlGuestApps.push(url_guest_app);
+        //const url_guest_app = await temp_page.$eval(url_guest_appSelector, el => el.href);
+        //urlGuestApps.push(url_guest_app);
 
         const service = services[i];
-        const hasVC = service === "expedia" || await temp_page.$eval(doesHaveVirtualCardTextareaDeterminatorSelector, 
-                el => /crédito virtual/.test(el.textContent.trim()));
+        const hasVC = await hasVCFunc(temp_page, service);
         const bookingTabAnchor = await temp_page.$(bookingTabAnchorSelector);
         await awaitClick(bookingTabAnchor, temp_page);
-        /*
-        const howLongStay = await temp_page.$eval(howManyNights, el => 
-            +el.childNodes[4].textContent.trim());
-        */
-        if(service === "booking" || service === "holidu" || service === "directas" || service === "expedia")
+        const VCServices = ["booking", "holidu", "directas", "expedia"]; 
+        if(VCServices.includes(service))
         {
-            const toPayAmout = await temp_page.$eval(toPayAmountSelector, el => 
-                +el.textContent.trim().slice(0, -2));
-            const difference = await temp_page.$eval(outstandingPaymentSelector, el => 
-                +el.textContent.trim().slice(0,-2));
-            const hasMadeDepositTransaction = await temp_page.$$eval(hasPaidTheDepositCandidateSelector, 
-                 async (els, departureDate) => {
-                const lastEl = els[els.length - 1];
-                const lastElNodes = lastEl.childNodes;
-                if(lastElNodes.length === 1)
-                {
-                    const lastElParent = lastEl.parentNode;
-                    const lastElParentFirstTd = lastElParent.childNodes[1];
-                    const paymentDateAsString = lastElParentFirstTd.textContent.trim();
-                    const paymentDateTrimmed = paymentDateAsString.slice(-10);
-                    const paymentDatePlusSevenDays = await nDaysFromGivenDate(paymentDateTrimmed, 7);
-                    const departureDateAsDate = await dateFromString(departureDate);
-                    return paymentDatePlusSevenDays > departureDateAsDate;
-                }
-                return false;
-            }, departureDate);
-            
-            const hasDepositExtra = await temp_page.$$eval(depositHasBenDeclaredDeterminantSelectorAll, els => {
-                var ret = false;
-                for(var i = 0; i < els.length; i++){
-                    const elsChildrenNodes = els[i].childNodes;
-                    const elsTitleNode = elsChildrenNodes[1].textContent.trim();
-                    const elsTitleNodeLC = elsTitleNode.toLowerCase();
-                    if(elsTitleNodeLC === "deposit" || 
-                       elsTitleNodeLC === "depósito" || 
-                       elsTitleNodeLC === "fianza" ||
-                       elsTitleNodeLC === "deposito")
-                        ret = true;
-                }
-                return ret;
-            });
-            if(hasVC)
-            {
-                const hasPaidDeposit = 
-                    ( hasDepositExtra && Math.abs(toPayAmout - difference) < INDIFFERENCE_AMOUNT) ||
-                    (!hasDepositExtra && Math.abs(toPayAmout - difference - DEPOSIT_AMOUNT) < INDIFFERENCE_AMOUNT) ||
-                    ( hasDepositExtra && Math.abs(difference) < INDIFFERENCE_AMOUNT) ||
-                    (!hasDepositExtra && Math.abs(difference + DEPOSIT_AMOUNT) < INDIFFERENCE_AMOUNT) ||
-                    hasMadeDepositTransaction;
-                hasPaidDeposits.push(hasPaidDeposit);
+            const toPayAmout                = await toPayAmountFunc(temp_page);
+            const outstandingPayment        = await outstandingPaymentFunc(temp_page);
+            const hasMadeDepositTransaction = await hasDepositTransactionFunc(temp_page, departureDate);
+            const hasDepositExtra           = await hasDepositExtraFunc(temp_page);
+
+            const hasPaidDeposit = hasPaidDepositFunc(hasVC, toPayAmout, outstandingPayment, 
+                hasMadeDepositTransaction, hasDepositExtra);
+
+            hasPaidDeposits.push(hasPaidDeposit);
+            if(hasVC){
                 hasPaidThePrices.push(true);
                 paymentDifferences.push(0);
-            }
-            else
-            {
-                const hasPaidDeposit = 
-                    ( hasDepositExtra && Math.abs(difference) < INDIFFERENCE_AMOUNT) ||
-                    (!hasDepositExtra && Math.abs(difference + DEPOSIT_AMOUNT) < INDIFFERENCE_AMOUNT) ||
-                    hasMadeDepositTransaction;
-                hasPaidDeposits.push(hasPaidDeposit);
-                hasPaidThePrices.push(difference < INDIFFERENCE_AMOUNT);
-                paymentDifferences.push(difference);
+            } else {
+                hasPaidThePrices.push(outstandingPayment < INDIFFERENCE_AMOUNT);
+                paymentDifferences.push(outstandingPayment);
             }
         }
         else if(service === "airbnb-online")
@@ -188,6 +235,7 @@ const scrape = async () => {
 
         const guestsTabAnchor = await temp_page.$(guestsTabAnchorSelector);
         await awaitClick(guestsTabAnchor, temp_page);
+
         const guestTrs = await temp_page.$$(guestTableRowSelectorAll);
         hasGuestsFIlledOut.push(!!guestTrs.length && guestTrs.length === noPersons);
         const passportNumbers = await temp_page.$$eval(tablePassportNumberSelector, els => 
@@ -219,42 +267,39 @@ const isPassportValid = str => {
     return charAcc !== str.length;
 }
 
-const apartmentCleaningTrSelectorAll = ".table-condensed > tbody > tr:not(.bg-light)";
-const cleanClassName = "text-success";
 const cleanApartamentNames = async browser => {
+    const apartmentCleaningTrSelAll = ".table-condensed > tbody > tr:not(.bg-light)";
+    const cleanClassName = "text-success";
     const cleaningsUri = "https://gero.icnea.net/HosOrdNetejes.aspx";
     const cleaning_page = await browser.newPage();
     await cleaning_page.goto(cleaningsUri, pageGotoOptions);
-    const apartmentObjects = await cleaning_page.$$eval(apartmentCleaningTrSelectorAll, 
-        (els, cleanClassName) => {
-            return els.map(el => {
-                const secondNode = el.childNodes[1]; 
-                const leafNode = secondNode.childNodes[1];
-                const textNode = el.childNodes[5].childNodes[1];
-                const textNodeContent = textNode.textContent.trim();
-                if(leafNode === undefined){
+    const apartmentObjects = await cleaning_page.$$eval(apartmentCleaningTrSelAll, (els, cleanClassName) => 
+        els.map(el => {
+            const secondNode = el.childNodes[1]; 
+            const leafNode = secondNode.childNodes[1];
+            const textNode = el.childNodes[5].childNodes[1];
+            const textNodeContent = textNode.textContent.trim();
+            if(leafNode === undefined){
+                return {
+                    aptName: textNodeContent.slice(1, -1),
+                    isClean: textNode.classList.contains(cleanClassName)
+                }
+            }
+            else {
+                const leafSpan = leafNode.childNodes[0];
+                if(leafSpan !== undefined)
                     return {
-                        aptName: textNodeContent.slice(1, -1),
-                        isClean: textNode.classList.contains(cleanClassName)
+                        aptName: textNodeContent,
+                        isClean: leafSpan.classList.contains(cleanClassName)
                     }
-                }
-                else {
-                    const leafSpan = leafNode.childNodes[0];
-                    if(leafSpan !== undefined)
-                        return {
-                            aptName: textNodeContent,
-                            isClean: leafSpan.classList.contains(cleanClassName)
-                        }
-                    else
-                        return {
-                            aptName: textNodeContent,
-                            isClean: leafNode.classList.contains(cleanClassName)
-                            
-                        }
-                }
-
-            });
-    }, cleanClassName);
+                else
+                    return {
+                        aptName: textNodeContent,
+                        isClean: leafNode.classList.contains(cleanClassName)
+                        
+                    }
+            }
+        }), cleanClassName);
     cleaning_page.close();
     return apartmentObjects.filter(el => el.isClean).map(el => el.aptName);
 };
